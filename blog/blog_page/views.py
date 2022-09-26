d1ab071ea4839a11b164d17a52569fcd0eed8aa6
MohammedSoubaneh <soubcompany@gmail.com>
2022-09-26 11:28:29 -0600

    Email Form

--- blog/blog_page/views.py
+++ blog/blog_page/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from .forms import EmailPostForm
 from .models import Post
 
 def post_list(request):
@@ -29,8 +30,26 @@ def post_detail(request, year, month, day, post):
                     {'post': post})
 
 
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status='published')
+
+    if request.method == 'POST':
+
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html',
+    {'post': post,
+    'form': form})
+
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
+
+
+
