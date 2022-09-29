11cff5e9175ae2639cdd9b9847f8bcf1594fcb29
MohammedSoubaneh <soubcompany@gmail.com>
2022-09-28 20:16:53 -0600

    Added Postgres DB

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -79,13 +79,15 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-       'default': {
-           'ENGINE': 'django.db.backends.postgresql',
-           'NAME': 'blog',
-           'USER': 'blog',
-           'PASSWORD': 'FakePassword321!',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "template1",
+        "USER": "mohammedsoubaneh",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
